72d6ddf320c1b2214624d25afc46b9b5b9f7d470
roszenil <roszenil@gmail.com>
2018-07-09 14:21:34 -0500

    Re-writing for new angiosperm database
    New angiosperm database tables. Trying to decide on a faster way

--- server.R
+++ server.R
@@ -3,53 +3,57 @@ library("ggplot2")
 library("stringr")
 
 # Loading data
-fern.dataset<-read.csv("fernccdbclean.csv") #partially clean chromosome number datasets
-haploid.number<-rep(0,11993) # This is the number of entries in this dirty dataset, it is larger in the full
-
-# This for loop calculates the haploid number based on whether there is an actual record of gametophytic number, but if absent haploid2 has sporophytic divided by 2 
-for (i in 1:11993){
-  if(!is.na(fern.dataset$haploid1[i])==TRUE){
-    haploid.number[i]<-fern.dataset$haploid1[i]}else{
-      if(!is.na(fern.dataset$haploid2[i])==TRUE)
-        haploid.number[i]<-fern.dataset$haploid2[i]else{
-          haploid.number[i]<-NA
-        }}}
-
-#Building a dataset with the haploid number
-fern.dataset<-cbind(fern.dataset,haploid.number)
-taxa<-unique(fern.dataset[,1]) #3353 is the total of different taxa
-smallest<-rep(0,3353)
-
-#This for loop looks into which is the smallest haploid number observed for each taxon. This is useful just to match with the phylogeny
-for(i in 1:3353){
-  aux<-which(fern.dataset[,1]==taxa[i])
-  aux2<-min(fern.dataset$haploid.number[aux])
-  smallest[i]<-aux2
-}
+#dataset.loaded<-read.csv("fernccdbclean.csv") #partially clean chromosome number datasets
+#haploid.number<-rep(0,11993) # This is the number of entries in this dirty dataset, it is larger in the full
+load("angiorecordsclean2018.RData")
+dataset.loaded<-angiorecordsclean2018
+# # This for loop calculates the haploid number based on whether there is an actual record of gametophytic number, but if absent haploid2 has sporophytic divided by 2 
+# for (i in 1:11993){
+#   if(!is.na(dataset.loaded$haploid1[i])==TRUE){
+#     haploid.number[i]<-dataset.loaded$haploid1[i]}else{
+#       if(!is.na(dataset.loaded$haploid2[i])==TRUE)
+#         haploid.number[i]<-dataset.loaded$haploid2[i]else{
+#           haploid.number[i]<-NA
+#         }}}
+# 
+# #Building a dataset with the haploid number
+# dataset.loaded<-cbind(dataset.loaded,haploid.number)
+# taxa<-unique(dataset.loaded[,1]) #3353 is the total of different taxa
+# smallest<-rep(0,3353)
+# 
+# #This for loop looks into which is the smallest haploid number observed for each taxon. This is useful just to match with the phylogeny
+# for(i in 1:3353){
+#   aux<-which(dataset.loaded[,1]==taxa[i])
+#   aux2<-min(dataset.loaded$haploid.number[aux])
+#   smallest[i]<-aux2
+# }
 
 #Building a small data set to match with the phylogeny
 smallest.dataset<-data.frame(taxa,smallest)
 
-
-#Remove NAs
-aux3<-which(!is.na(smallest.dataset$smallest))
-smallest.dataset<-smallest.dataset[aux3,]#3041
+# 
+# #Remove NAs
+# aux3<-which(!is.na(smallest.dataset$smallest))
+# smallest.dataset<-smallest.dataset[aux3,]#3041
 
 #Create individual genus
-genus<-unique(fern.dataset$genus)#362
-
-
+#genus<-unique(dataset.loaded$genus)#362
+genus<-unique(dataset.loaded$Genus)
+removing.na<-which(is.na(genus)==TRUE)
+genus=genus[-removing.na]
+long1<-length(genus)
 # I'm making 2 lists one that helped building the tables and the histograms with information
 per.genus.counts<-list()
 per.genus.table<-list() #293
-for(j in 1:362){
-  aux4<-which(fern.dataset$genus==genus[j])
-  per.genus.counts[[j]]<-data.frame(species=as.character(fern.dataset$resolvedchromer[aux4]), haploid.number=fern.dataset$haploid.number[aux4])
-  per.genus.table[[j]]<-table(per.genus.counts[[j]]$species, per.genus.counts[[j]]$haploid.number)
+
+for(j in 1:long1){
+  print(j)
+  aux4<-which(dataset.loaded$Genus==genus[j])
+  per.genus.counts[[j]]<-data.frame(species=as.character(dataset.loaded$Species[aux4]), haploid.number=dataset.loaded$HaploidNumber[aux4])
+  per.genus.table[[j]]<-table(paste(genus[j],per.genus.counts[[j]]$species,sep="_"), per.genus.counts[[j]]$haploid.number)
 }
 names(per.genus.counts)<-genus
 
-per.genus.table
 
 # Function that takes in one original array of strings and an array of string to remove from the original one
 stringRemover <- function(originalArray, arrayOfStringsToRemove){
