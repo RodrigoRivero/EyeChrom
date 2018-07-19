20b145e8a8496c54cec41275f2d14b0df88b14b3
Rodrigo Rivero <rodrigoriverocastro@gmail.com>
2018-07-18 20:27:41 -1000

    Added download and select button functionality

--- server.R
+++ server.R
@@ -51,7 +51,8 @@ names(per.genus.counts)<-genus
 
 per.genus.table
 
-# Function that takes in one original array of strings and an array of string to remove from the original one
+# There is some limitation from what can be done by a shiny application, so somefunctions needed to be written. These are them:
+# Function that takes in one original vector of strings and a vector of strings to remove from the original one
 stringRemover <- function(originalArray, arrayOfStringsToRemove){
   toRemove <- vector()
   for(j in 1:length(arrayOfStringsToRemove)){
@@ -66,7 +67,7 @@ stringRemover <- function(originalArray, arrayOfStringsToRemove){
   newArray <- originalArray[-toRemove]
   return(newArray)
 }
-
+# This function puts the correct columns names to a data frame, removes records with a 0, and fixes names with by removing "_"
 putTheColumnNames <- function(dataFrame){
   newFrame <- dataFrame
   colnames(newFrame) <-  c("species", "chromosome", "freq")
@@ -74,69 +75,98 @@ putTheColumnNames <- function(dataFrame){
   newFrame$species <- gsub("_", " ", newFrame$species) # fixing names to remove the underscor in names
   return(newFrame)
 }
+# This other funtions just puts the proper names, used for the table not the chart
+putTheColumnNamesForTable <- function(dataFrame){
+  newFrame <- dataFrame
+  colnames(newFrame) <-  c("Species", "Chromosome Number", "Number Of Chromosomes")
+  return(newFrame)
+}
+
+# This functions takes the dataframe for the genus and the list of checked species to see. Returns a new data frame without any unchecked species
+onlyChecked <- function(dataFrame, listChecked){
+  listToRemove <- vector()
+  searchForList <- listChecked
+  species <- unique(dataFrame$species) 
+  if (length(searchForList) != 0 && length(searchForList) != length(species)){ # only happens if any boxes are checked and if not all of them are checked 
+    searchForList <- stringRemover(species,searchForList) # removes the list that we want from the total, getting the list we dont want
+    for(j in 1:length(searchForList)){ # happens each time per species to remove
+      searchFor <- searchForList[j] # species to remove
+      for(i in 1:length(dataFrame$species)){ # runs through all species in this genus
+        thisLine <- dataFrame$species[i] # gets the row
+        if (str_detect(thisLine,searchFor)){ # if the species we want to remove is in this row
+          listToRemove <- c(listToRemove,i)# the row numbers gets added to a list
+        }
+      }
+    }
+    dataFrame <- dataFrame[-listToRemove,] # removes list unwanted from total list
+  }
+  return(dataFrame)
+}
+
+# A function that calculates the right margin for the ggplot
+correctMargin <- function(dataFrame){
+  bottomMargin <- 1000
+  if (length(dataFrame$species) > 20) { bottomMargin <- 800}
+  if (length(dataFrame$species) > 30) { bottomMargin <- 600}
+  if (length(dataFrame$species) > 40) { bottomMargin <- 400}
+  return(bottomMargin)
+}
 
 #--------------------------All above is backend stuff-------------------------#
 #--------------------------All below is connected to the UI-------------------#
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
+  
   # Getting the name of the current Genus
-  GenusName <-reactive({
-    genus[as.numeric(input$var)]
+  GenusName <-reactive({ genus[as.numeric(input$var)] })
+  
+  # Button for selecting/disselecting all
+  output$select <- renderUI({
+    actionButton("select", label = "Select/Disselect All")  
   })
   
-  # Getting the data frame of the current Genus
+  # Getting the data frame of the current Genus. Uses the column names function.
   GenusSubset <-reactive({
-    putTheColumnNames(as.data.frame(per.genus.table[[as.numeric(input$var)]]))# ggplot needs a data frame to work
+    putTheColumnNames(as.data.frame(per.genus.table[[as.numeric(input$var)]]))
   })
-  
-  
   
   # Creating the Checkboxes that change for each genus
   output$speciesControls <- renderUI({ 
     mySubset <- GenusSubset() # Getting subset
-    species <- unique(mySubset$species) # creating vector of species
-    checkboxGroupInput("cities", "Choose Species", species, selected = species) # creating checkboxes
+    species <- unique(mySubset$species)# creating vector of species
+    checked <- species
+    selected <- input$select %% 2 # this alternates the select/disselect button
+    if (selected == 0){ checked <- NULL}
+    checkboxGroupInput("speciesChecked", "Choose Species", species, selected = checked)
+    # creating checkboxes
   })
-  
-  output$value <- renderPrint({ input$cities })
   
   output$distPlot <- renderPlot({ # This create the output plot to be sent to the UI
     genusName <- GenusName() # Getting name of Genus
-    mySubset <- GenusSubset() # Getting subset
-    searchForList <- input$cities # list of species we want to see
-    species <- unique(mySubset$species) # list of all species
-    listToRemove <- vector() # vector that eventually will have the list of rows to remove
-    # Start of if and for statements that take the information from the checkboxes
-    if (length(searchForList) != 0 && length(searchForList) != length(species)){ # only happens if any boxes are checked and if not all of them are checked 
-      searchForList <- stringRemover(species,searchForList) # removes the list that we want from the total, getting the list we dont want
-    for(j in 1:length(searchForList)){ # happens each time per species to remove
-      searchFor <- searchForList[j] # species to remove
-      for(i in 1:length(mySubset$species)){ # runs through all species in this genus
-        thisLine <- mySubset[i,] # gets the row
-        if (str_detect(thisLine$species,searchFor)){ # if the species we want to remove is in this row
-          listToRemove <- c(listToRemove,i) # the row numbers gets added to a list
-        }
-      }
-    }
-      mySubset <- mySubset[-listToRemove,] # removes list unwanted from total list
-    }
-    myTitle <- paste("Chromosome Data For", genusName) # title name generated dinamically with the choice of the user
-     # proper names
-    speciesNames <- as.list(mySubset$species)
-## Plotting 
+    mySubset <- onlyChecked(GenusSubset(),input$speciesChecked) # Getting subset to plot. Uses function only to get checked species
+    # Plotting Time
     ggplot(data = mySubset, aes(x = chromosome, y = freq, fill = species)) + #fill species indicates it will be a stacker bar graph, as well as handling different colors
       geom_bar(stat="identity") + 
-      labs(title = myTitle, 
+      labs(title = paste("Chromosome Data For", genusName), # Title gets generated dinamically
            y = "Number Of Records", 
            x = "Chromosome Number", 
            fill = "Species Name")+ # data for the axis and legend
       scale_fill_hue(l=60)+ # color adjustment
-      theme(plot.title = element_text(hjust = 0.5, vjust = 0), legend.position = "bottom") # centering title
-  }, height = 700)
+      theme(plot.title = element_text(hjust = 0.5, vjust = 0), plot.margin = margin(0,0,correctMargin(mySubset),0), legend.position = "bottom") # centering title. Also calculates the proper bottom margin to have so that the legend shows properly.
+  }, height = 1500)
   
+  # output table to be downloaded
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(GenusName(), ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(onlyChecked(GenusSubset(),input$speciesChecked), file, row.names = FALSE)
+    }
+  )
+  
+  # Creating table
   output$table <- renderTable({
-    as.data.frame(GenusSubset())
+    putTheColumnNamesForTable(as.data.frame(GenusSubset()))
   })
-  
 })
